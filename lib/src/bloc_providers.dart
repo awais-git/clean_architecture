30d073486df0b5c2aa0c6f044ba8abacb24dd79a
Awais Sarwar khan <awais.sarwar@evampsaanga.com>
2023-06-12 16:55:42 +0500

    fixes

---
+++ lib/src/bloc_providers.dart
@@ -0,0 +1,15 @@
+// import 'package:clean_artitecture/src/locator.dart';
+// import 'package:clean_artitecture/src/presentation/cubits/remote_articles/remote_articles_cubit.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// import 'domain/repositories/api_repository.dart';
+
+// class BlocProviders {
+//   List <MultiBlocProvider> blocProviders = [
+//     BlocProvider(
+//       create: (context) => RemoteArticlesCubit(
+//         locator<BreakingNewsRepository>(),
+//       )..getBreakingNewsArticles(),
+//     )
+//   ];
+// }
